744092c844163a6c7957586b94ea607a901f1041
DJ_Yazid <y.djaoui@rubika-edu.com>
2024-04-29 15:04:32 +0200

    LogsTrigger

--- ProjetFirstPerson/Assets/SteleScript.cs
+++ ProjetFirstPerson/Assets/SteleScript.cs
@@ -5,7 +5,22 @@ using UnityEngine;
 public class SteleScript : MonoBehaviour
 {
     [Header("References")]
+    private LogsMenu logsMenu;
     public bool isAlreadyInLogs;
     [SerializeField]  public string titleLogs;
     [SerializeField] [TextArea(5,10)] public string myInfo;
+    
+    void Start()
+    {
+        logsMenu = GameObject.Find("TabletteManager").GetComponent<LogsMenu>();
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Player") && !isAlreadyInLogs)
+        {
+            isAlreadyInLogs = true;
+            logsMenu.AddLogsToContent(myInfo,titleLogs);
+        }
+    }
 }
