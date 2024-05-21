5badda40527d14075d602352230079ce1601f948
Arthur <a.perignon@rubika-edu.com>
2024-05-21 10:49:15 +0200

    Polish créature uwu

--- ProjetFirstPerson/Assets/Scripts/Creature/CreatureLegsMover.cs
+++ ProjetFirstPerson/Assets/Scripts/Creature/CreatureLegsMover.cs
@@ -163,10 +163,10 @@ namespace Creature
 
                 else
                 {
-                    if (currentLeg.isFrontLeg && distOriginTarget > data.maxFrontLegDistWalk * 0.85f)
+                    if (currentLeg.isFrontLeg && distOriginTarget > data.maxFrontLegDistWalk * 0.8f)
                         return true;
 
-                    if (!currentLeg.isFrontLeg && distOriginTarget > data.maxBackLegDistWalk * 0.85f)
+                    if (!currentLeg.isFrontLeg && distOriginTarget > data.maxBackLegDistWalk * 0.8f)
                         return true;
                 }
             }
@@ -211,7 +211,7 @@ namespace Creature
                 {
                     float dist = Vector3.Distance(hit.point, currentTargetPos);
 
-                    if (dist > currentMax && Vector3.Distance(hit.point, origin) < legMaxDist * 1f)
+                    if (dist > currentMax && Vector3.Distance(hit.point, origin) < legMaxDist * 1.1f)
                     {
                         currentMax = dist;
                         chosenPos = hit.point;
