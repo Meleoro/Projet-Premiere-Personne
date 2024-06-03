d64f7d2e03ceb4498166571bbb86586aa7346e00
Arthur <arthur.perigno@gmail.com>
2024-06-03 22:26:05 +0200

    Trigger poursuite finale

---
+++ ProjetFirstPerson/Assets/Scripts/TriggerPoursuiteFinale.cs
@@ -0,0 +1,63 @@
+using Creature;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPoursuiteFinale : MonoBehaviour
+{
+    [Header("Private Infos")]
+    private List<Vector3> originalPosition = new List<Vector3>();
+
+    [Header("References")]
+    [SerializeField] private List<Transform> objectsToPutBack = new List<Transform>();
+    [SerializeField] private CreatureTrigger triggerToReset;
+    [SerializeField] private Transform creatureTPPos;
+    [SerializeField] private CreatureWaypoints creatureToMove;
+
+    private void Start()
+    {
+        for(int i = 0; i < objectsToPutBack.Count; i++)
+        {
+            originalPosition.Add(objectsToPutBack[i].position);
+        }
+    }
+
+
+    public void ActivateTrigger()
+    {
+        // We put back the objects
+        for (int i = 0; i < objectsToPutBack.Count; i++)
+        {
+            objectsToPutBack[i].position = originalPosition[i];
+        }
+
+        // We reactivate the trigger
+        triggerToReset.gameObject.SetActive(true);
+
+        // We move the creature
+        creatureToMove.transform.GetComponent<CreatureMover>().navMeshAgent.enabled = false;
+        Vector3 moveDir = creatureTPPos.position - creatureToMove.transform.position;
+        creatureToMove.transform.parent.transform.position += moveDir;
+
+        creatureToMove.transform.GetComponent<CreatureMover>().tailIKScript.RebootTargets();
+
+        creatureToMove.transform.GetComponent<CreatureMover>().navMeshAgent.enabled = true;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<HealthComponent>().currentTriggerPoursuiteF = this;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<HealthComponent>().currentTriggerPoursuiteF = null;
+        }
+    }
+}
