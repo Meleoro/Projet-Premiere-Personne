ada993d12bae0348d16b79271928869963c876b8
Arthur <a.perignon@rubika-edu.com>
2024-06-10 12:45:28 +0200

    Upgrade ik jambe hehe

--- ProjetFirstPerson/Assets/Scripts/Creature/CreatureLegsMover.cs
+++ ProjetFirstPerson/Assets/Scripts/Creature/CreatureLegsMover.cs
@@ -344,7 +344,10 @@ namespace Creature
                     if(forceFront)
                         dist = Vector3.Distance(hit.point, mainTrRotRefBack.position);
 
-                    if (dist > currentMax && Vector3.Distance(hit.point, origin) < legMaxDist * 1.05f)
+                    float maxDistMultiplier = Mathf.Lerp(0.9f, 1.05f,
+                        creatureMover.navMeshAgent.velocity.magnitude / creatureMover.navMeshAgent.speed);
+                    
+                    if (dist > currentMax && Vector3.Distance(hit.point, origin) < legMaxDist * maxDistMultiplier)
                     {
                         currentMax = dist;
                         chosenPos = hit.point;
