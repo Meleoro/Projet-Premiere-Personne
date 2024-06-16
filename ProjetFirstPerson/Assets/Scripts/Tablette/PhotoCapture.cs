317798124993dbf666e233a1b7e22c93ab875cfb
DJ_Yazid <y.djaoui@rubika-edu.com>
2024-06-16 20:01:58 +0200

    Debug à l'aveugle

--- ProjetFirstPerson/Assets/Scripts/Tablette/PhotoCapture.cs
+++ ProjetFirstPerson/Assets/Scripts/Tablette/PhotoCapture.cs
@@ -66,7 +66,7 @@ public List<MyPhoto> MyPhotos = new List<MyPhoto>();
 
         ScreenRectTransform = ScreenPhotoImage.GetComponent<RectTransform>().rect;
 
-        ScreenDetectionLogs = GameObject.Find("DetectPhotoScreen");
+      //  ScreenDetectionLogs = GameObject.Find("DetectPhotoScreen");
     }
     void OnDrawGizmos()
     {
